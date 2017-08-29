48f5b702362ae2b87b5bf4357d11f92d1c69f8b3
Jeremy Smith <j.smith.03@cantab.net>
2017-08-28 18:18:49 -0700

    Port correction

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    port = int(os.environ.get("PORT", 33507))
+    app.run(port=port)
